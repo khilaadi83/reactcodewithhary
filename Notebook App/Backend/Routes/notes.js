bd0af435015d7c5c399db38805f594fcc4064443
Kartik Narang <kartik.narang@borngroup.com>
2024-07-08 17:16:44 +0530

    additional notes

--- Notebook App/Backend/Routes/notes.js
+++ Notebook App/Backend/Routes/notes.js
@@ -51,22 +51,43 @@ router.post('/updateNotes/:id', body('title', 'Minimum 6 Character Required').is
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         } try {
-        const { title, description } = req.body;
-        var newNote = {};
-        if (title ? newNote.title = title : null);
-        if (description ? newNote.description = description : null);
-        var note = await Notes.findByIdAndUpdate(req.params.id);
+            const { title, description } = req.body;
+            var newNote = {};
+            if (title ? newNote.title = title : null);
+            if (description ? newNote.description = description : null);
+            var note = await Notes.findByIdAndUpdate(req.params.id);
+            if (!note) {
+                return res.status(404).json({ msg: 'Note not found' });
+            }
+            if (note.user.toString() !== req.authData.user._id.toString()) {
+                return res.status(401).json({ msg: 'Not Authorized to Update this Note' });
+            }
+            note = await Notes.findByIdAndUpdate(req.params.id, newNote, { new: true });
+            res.json({ msg: 'Note Updated Successfully', note: note });
+        } catch (e) {
+            res.status(500).json({ msg: 'Something Went Wrong While Updating the Note', error: e.message });
+        }
+    })
+
+// Route 4: Delete a node based on the id
+router.post('/deleteNote/:id', loginAuth.verifyToken, async (req, res) => {
+    try {
+        var note = await Notes.findById(req.params.id);
         if (!note) {
             return res.status(404).json({ msg: 'Note not found' });
         }
         if (note.user.toString() !== req.authData.user._id.toString()) {
             return res.status(401).json({ msg: 'Not Authorized to Update this Note' });
         }
-        note = await Notes.findByIdAndUpdate(req.params.id, newNote, { new: true });
-        res.json({ msg: 'Note Updated Successfully', note: note });
-    } catch(e){
-        res.status(500).json({ msg: 'Something Went Wrong While Updating the Note', error: e.message });
+
+        var deletedNote = await Notes.findByIdAndDelete(req.params.id);
+
+        res.json({ msg: 'Note Deleted Successfully', note: deletedNote });
+
+    } catch (e) {
+        res.status(500).json({ msg: 'Something Went Wrong While Deleting the Note', error: e.message });
     }
-    })
+
+})
 
 module.exports = router;
